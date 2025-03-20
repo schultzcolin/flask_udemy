c1a68833d406fc8747e9fbc6625ab890c8946750
colin schultz <schultz.colin@gmail.com>
2025-03-20 14:05:28 -0700

    first commmit

---
+++ app.py
@@ -0,0 +1,45 @@
+# First lesson on flask api from udemy course I am taking
+# basic functionality, we will not be storing our data in memory 
+# created on 3/20/25 
+
+from flask import Flask, request
+app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My Store", 
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_stores(): 
+    return {"stores": stores}
+
+@app.post("store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data['name'], "items": []}
+    return new_store, 201 
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {'name': request_data['name'], "price": request_data['price']}
+            store['items'].append(new_item)
+            return new_item, 201
+    return{"message": "store not found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return {"items": store['items']}
+    return {"message": "store not found"}, 404 
