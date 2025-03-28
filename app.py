b6bb793996c58f9868adce8704d9edc053ad3136
colin schultz <schultz.colin@gmail.com>
2025-03-28 16:25:05 -0700

    Finally sorted out bugs, learned more about docker to get it to run.

--- app.py
+++ app.py
@@ -22,14 +22,13 @@ def create_app(db_url=None):
     app.config['OPENAPI_URL_PREFIX'] = "/"
     app.config['OPENAPI_SWAGGER_UI_PATH'] = "/swagger-ui"
     app.config['OPENAPI_SWAGGER_UI_URL'] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url or os.getenv("DATABASE_URL", "sqllite:///data.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     api = Api(app)
 
-    @app.before_first_request
-    def create_tables():
-        db.create_all()
+    with app.app_context():
+     db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
